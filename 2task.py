b8b0a6f833af0e9dbd5bb8a4dcbb241212ad0f1a
Rubikalakshmanan <152481460+Rubikalakshmanan@users.noreply.github.com>
2023-12-04 20:53:37 +0530

    Add files via upload

---
+++ 2task.py
@@ -0,0 +1,24 @@
+num1=20
+num2=30
+name="Rubika"
+num=10
+print("********************")
+print("variable Example")
+print("*********************")
+print(num1+num2)
+print(num1-num2)
+print(num1**num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+print("find type function")
+print("*************************")
+print(type(num1))
+print(type(name))
+print("*****************")
+print("Id() eg")
+print("*********")
+print(id(num1))
+print(id(num2))
+print(id(name))
+print(id(num))
